4275f77bb41571d86e24aa5f0b4732bd312feb76
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-21 21:47:47 +0800

    Add the instruction and pseudocode

---
+++ display_numbers_divisible_by_five.py
@@ -0,0 +1,11 @@
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# pseudocode
+
+# create the given list of numbers
+
+# iterate the list of numbers
+
+# check if the number is divisible by 5
+
+# if yes, print the number
