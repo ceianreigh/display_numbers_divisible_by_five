ff000e71ac9e239280444492a40d2040538f86fa
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-21 21:52:45 +0800

    Add print statements to display given list and numbers divisible by 5

--- display_numbers_divisible_by_five.py
+++ display_numbers_divisible_by_five.py
@@ -5,6 +5,10 @@
 # create the given list of numbers
 numbers = [10, 20, 33, 46, 55]
 
+# add message to print the given list and numbers divisible by 5
+print("Given list of numbers: ", numbers)
+print("Numbers divisible by 5:")
+
 # iterate the list of numbers
 
 # check if the number is divisible by 5
